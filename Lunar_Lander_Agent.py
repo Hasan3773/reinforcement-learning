b0343853a8656e1272789400f9513d16fb320669
Hasan3773 <88059238+Hasan3773@users.noreply.github.com>
2024-01-11 02:12:21 -0500

    Create Lunar_Lander_Agent.py
    Added lunar lander agent

---
+++ Lunar_Lander_Agent.py
@@ -0,0 +1,41 @@
+import gymnasium as gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+
+# Then we reset this environment
+observation, info = env.reset()
+
+# Create the environment
+env = make_vec_env('LunarLander-v2', n_envs=16)
+
+# Instantiate the agent
+model = PPO('MlpPolicy', env, verbose=1)
+# Train the agent
+model.learn(total_timesteps=int(2e5))
+
+model = PPO(
+    policy = 'MlpPolicy',
+    env = env,
+    n_steps = 1024,
+    batch_size = 64,
+    n_epochs = 4,
+    gamma = 0.999,
+    gae_lambda = 0.98,
+    ent_coef = 0.01,
+    verbose=1)
+
+# Train it for 1,000,000 timesteps
+model.learn(total_timesteps=1000000)
+# Save the model
+model_name = "ppo-LunarLander-v2"
+model.save(model_name)
+
+# Evaluate model 
+eval_env = Monitor(gym.make("LunarLander-v2"))
+mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10, deterministic=True)
+print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+env.close()
